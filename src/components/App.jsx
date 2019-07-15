380176b813a01d224d531ea4ef9de6d271a122c2
Valery Romanov <ikrakozabra@gmail.com>
2019-07-15 17:29:06 +0300

    :tada: Initial commit for 2.0 branch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,12 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router'
-import { connect } from 'react-redux'
-
-import './styles/App.css';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
-const currentPageIsHome = () => window.location.hash === '#/'
+import React, { Component } from 'react'
+import './App.scss'
 
 class App extends Component {
-  render() {
-    let { quentas } = this.props.store.quentaReducer
-    let qLinks = quentas
-      .map((q, i) => (
-        <li key={i} className={window.location.hash.match('.*' + q.name) ? 'active' : ''}>
-          <Link to={`quenta/${q.name}`}>{q.name}</Link>
-        </li>)
-      )
+  return (
+    <div className="application-layout">
 
-    return (
-      <div className="container-fluid App">
-        <div className="row">
-          <div className="col-sm-3 col-md-3 col-lg-3 sidebar">
-            <Link to="/" className="btn btn-success" id="new-quenta-btn" style={{display: currentPageIsHome() ? 'none' : '' }}>
-              <span className="glyphicon glyphicon-plus"></span>
-            </Link>
-            <Link to="settings" className="btn btn-default" id="settings-btn" style={{ borderRadius: currentPageIsHome() ? '0 0 10px 10px' : '0 0 10px 0' }}>
-              <span className="glyphicon glyphicon-cog"></span>
-            </Link>
-            <ul className="nav nav-sidebar" id="quenta-linklist">
-              {qLinks}
-            </ul>
-          </div>
-
-          <div className="col-sm-9 col-md-9 col-lg-9 main">
-            {this.props.children}
-          </div>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  )
 }
 
-export default connect(state => ({ store: state }))(App)
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import './index.scss'
 import App from './components/App'
-import Settings from './components/Settings'
-import { Provider } from 'react-redux'
-import CharProfile from './components/CharProfile'
-import NewProfile from './components/NewProfile'
-import store from './store'
-import Quenta from './model/quenta'
-import { getSkillsets } from './logic/skillset-provider'
-import { getRaces } from './logic/race-provider'
-import { ACTION_TYPES as skillActions } from './reducers/skill-reducer'
-import { ACTION_TYPES as raceActions } from './reducers/race-reducer'
-import { ACTION_TYPES as quentaActions } from './reducers/quenta-reducer'
-import './index.css'
 
-getSkillsets('vanilla').then(sets => store.dispatch({ type: skillActions.SKILLSETS_LOADED, payload: sets }))
-getRaces('vanilla').then(races => store.dispatch({ type: raceActions.RACES_LOADED, payload: races }))
-let savedQuentas = JSON.parse(localStorage.getItem('quentas')) || []
-savedQuentas = savedQuentas.map(Quenta.fromPlainObject)
-store.dispatch({ type: quentaActions.QUENTAS_LOADED, payload: savedQuentas })
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={NewProfile}></IndexRoute>
-        <Route path="quenta/:name" component={CharProfile}></Route>
-        <Route path="settings" component={Settings} />
-      </Route>      
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
