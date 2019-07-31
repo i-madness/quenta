8fdf12417f2e446ca5e384fdc1c9c4dd1f3ec2fa
Valery Romanov <ikrakozabra@gmail.com>
2019-08-01 00:07:57 +0300

    gh-pages branch

---
+++ precache-manifest.afd172dd0474d84546f5e674a705d412.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ab98fe30ae6d83a7d5b9fcb8da2929",
+    "url": "/quenta/index.html"
+  },
+  {
+    "revision": "33b86557172b79dd9ac6",
+    "url": "/quenta/static/css/main.ba398ea5.chunk.css"
+  },
+  {
+    "revision": "c63d85398e1a472bd830",
+    "url": "/quenta/static/js/2.72912114.chunk.js"
+  },
+  {
+    "revision": "33b86557172b79dd9ac6",
+    "url": "/quenta/static/js/main.9d313fe8.chunk.js"
+  },
+  {
+    "revision": "93797c31322fbf42cf92",
+    "url": "/quenta/static/js/runtime~main.215bd716.js"
+  },
+  {
+    "revision": "c5eb54404c9dbda925f7142d6343c913",
+    "url": "/quenta/static/media/Lato-Regular.c5eb5440.ttf"
+  },
+  {
+    "revision": "0c2c26015551ca2b1b89a8692316bc15",
+    "url": "/quenta/static/media/alegreya_regular.0c2c2601.ttf"
+  },
+  {
+    "revision": "59a17fc9a860eee4a9704eb870b6c1b8",
+    "url": "/quenta/static/media/alegreya_regular.59a17fc9.svg"
+  },
+  {
+    "revision": "70d32ecf5ea94465f845f156daff0dcb",
+    "url": "/quenta/static/media/alegreya_regular.70d32ecf.woff2"
+  },
+  {
+    "revision": "85d551785220215fb8be78406fe1a97c",
+    "url": "/quenta/static/media/alegreya_regular.85d55178.eot"
+  },
+  {
+    "revision": "8f262cbc8466a4643030c68f5b18c89e",
+    "url": "/quenta/static/media/alegreya_regular.8f262cbc.woff"
+  }
+]);
