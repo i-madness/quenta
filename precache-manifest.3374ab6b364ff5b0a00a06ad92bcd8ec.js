7103520efaec44cb31cc8ee50c730f2f37a5a368
Valery Romanov <ikrakozabra@gmail.com>
2020-02-17 02:50:34 +0300

    :ghost: Upd gh-pages

---
+++ precache-manifest.3374ab6b364ff5b0a00a06ad92bcd8ec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b9744fdc4f6812938be4b10c4622a7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcfe25e540cc46ec6df0",
+    "url": "/static/css/main.dd963d40.chunk.css"
+  },
+  {
+    "revision": "63ee470332789fd95fe8",
+    "url": "/static/js/2.2a406bef.chunk.js"
+  },
+  {
+    "revision": "bcfe25e540cc46ec6df0",
+    "url": "/static/js/main.3bc90814.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c5eb54404c9dbda925f7142d6343c913",
+    "url": "/static/media/Lato-Regular.c5eb5440.ttf"
+  },
+  {
+    "revision": "0c2c26015551ca2b1b89a8692316bc15",
+    "url": "/static/media/alegreya_regular.0c2c2601.ttf"
+  },
+  {
+    "revision": "417b6a71ce2d3bc86ecfeed8643b3a42",
+    "url": "/static/media/alegreya_regular.417b6a71.svg"
+  },
+  {
+    "revision": "70d32ecf5ea94465f845f156daff0dcb",
+    "url": "/static/media/alegreya_regular.70d32ecf.woff2"
+  },
+  {
+    "revision": "85d551785220215fb8be78406fe1a97c",
+    "url": "/static/media/alegreya_regular.85d55178.eot"
+  },
+  {
+    "revision": "8f262cbc8466a4643030c68f5b18c89e",
+    "url": "/static/media/alegreya_regular.8f262cbc.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quenta/precache-manifest.763d3b2c9e70e18e43373c75bf0c7033.js"
+  "/precache-manifest.3374ab6b364ff5b0a00a06ad92bcd8ec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quenta/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
